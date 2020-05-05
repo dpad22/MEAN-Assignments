aa4e9c3b824a82edbfad3721176ff6c7b9ed1954
David Padilla <dpadman2@gmail.com>
2020-05-05 14:19:24 -0500

    travels assignment

--- Pointers.js
+++ Pointers.js
@@ -10,6 +10,7 @@ var gopher = {character: "Gopher"};
 var kanga = {character: "Kanga"};
 var eeyore = {character: "Eeyore"};
 var heffalumpa = {character: "Heffalump"};
+var player = {location: tigger}
 
 
 
@@ -18,14 +19,14 @@ pooh.south = tigger;
 pooh.west = piglet;
 pooh.east = bees;
 pooh.north = chris_robin;
-piglet.east = tigger.north;
+piglet.east = pooh;
 piglet.north = owl;          // Piglet's east attribute is Tigger's north attribute, which is a memory address
-tigger.north.west = piglet;          // Follow Tigger's north attribute to a location in memory
+// tigger.north.west = piglet;          // Follow Tigger's north attribute to a location in memory
                                     // Assign that object's west attribute to piglet
 
 // bees pointers
 bees.west = pooh;
-bees.west.south = tigger;
+// bees.west.south = tigger;
 bees.north = rabbit;
 // rabbit pointers
 rabbit.south = bees;
@@ -49,3 +50,63 @@ eeyore.south = kanga;
 eeyore.east = heffalumpa;
 // heffalump pointer
 heffalumpa.west = eeyore;
+
+function move(direction){
+    if(direction == "north" || direction == "North"){
+        if(player.location.north == null){
+            console.log("The path to the North leads to nowhere. You turn around and head back!")
+        }
+        else {
+            player.location = player.location.north;
+            console.log("Your travels brings you to " + player.location.character + "'s house!");
+        }
+    }
+    if(direction == "east" || direction == "East"){
+        if(player.location.east == null){
+            console.log("The path to the East leads to nowhere. You turn around and head back!")
+        }
+        else {
+            player.location = player.location.east;
+            console.log("Your travels East brings you to " + player.location.character + "'s house!");
+        }
+    }
+    if(direction == "west" || direction == "West"){
+        if(player.location.west == null){
+            console.log("The path to the West leads to nowhere. You turn around and head back!")
+        }
+        else {
+            player.location = player.location.west;
+            console.log("Your travels West brings you to " + player.location.character + "'s house!");
+        }
+    }
+    if(direction == "south" || direction == "South"){3
+        if(player.location.south == null){
+            console.log("The path to the South leads to nowhere. You turn around and head back!")
+        }
+        else {
+            player.location = player.location.south;
+            console.log("Your travels South brings you to " + player.location.character + "'s house!");
+        }
+    }
+}
+
+move("north")
+move("west")
+move("south")
+move("east")
+
+// attempt to simplify function
+
+// function move(direction){
+//     // if(direction == "north" || direction == "North"){
+//         dir = ["north", "south", "east", "west"];
+//         console.log(dir.length)
+//         for (var i=0;i<dir.length;i++);
+//             console.log(dir[i]);
+//             if(player.location.dir[i] == null){
+//                 console.log("The path to the North leads to nowhere. You turn around and head back!")
+//             }
+//             else {
+//                 player.location = player.location.dir[i];
+//                 console.log("Your travels " + dir[i] + " brings you to " + player.location.character + "'s house!");
+//             }
